c1c8ba3d292dc0a93a4f29bfd8d00f81d8c507aa
kushalkumar1362 <kushalkumar1362@gmail.com>
2023-09-01 21:12:55 +0530

    Full Pyramid

--- FullPyramid.cpp
+++ FullPyramid.cpp
@@ -25,5 +25,20 @@ int main()
         cout << endl;
     }
 
+    for (int row = 0; row < num; row++)
+    {
+        for(int space = 0; space < row; space++)
+        {
+            cout << " ";
+        }
+        for (int col = 0; col < num - row  ; col++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+   
+
+
     return 0;
 }

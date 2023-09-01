03ced5cc893903d93bda5f54bdb5ccd2c2a40693
kushalkumar1362 <kushalkumar1362@gmail.com>
2023-09-01 13:20:48 +0530

    Dimond

---
+++ Dimond.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+/*
+    * 
+   * *
+  * * *
+ * * * *
+* * * * *       Dimond
+* * * * *       Pattern
+ * * * *
+  * * * 
+   * *
+    *
+ */
+int main()
+{
+    int num;
+    cout << "Enter number of rows : ";
+    cin >> num;
+
+    for (int row = 0; row < num; row++)
+    {
+        for (int space = 0; space < num - row - 1; space++)
+        {
+            cout << " ";
+        }
+        for (int col = 0; col < row + 1; col++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int row = 0; row < num; row++)
+    {
+        for (int space = 0; space < row; space++)
+        {
+            cout << " ";
+        }
+        for (int col = 0; col < num - row; col++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

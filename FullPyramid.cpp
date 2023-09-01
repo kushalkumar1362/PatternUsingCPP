6a92dc6e0deaea3054def6151b058a1fc04b02be
kushalkumar1362 <kushalkumar1362@gmail.com>
2023-09-01 13:07:04 +0530

    Full Pyramid

---
+++ FullPyramid.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+/*
+    * 
+   * *      Full Pyramid
+  * * *     Pattern
+ * * * *
+* * * * *
+ */
+int main()
+{
+    int num;
+    cout << "Enter number of rows : ";
+    cin >> num;
+    for (int row = 0; row < num; row++)
+    {
+        for(int space = 0; space < num - row -1; space++)
+        {
+            cout << " ";
+        }
+        for (int col = 0; col < row + 1 ; col++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

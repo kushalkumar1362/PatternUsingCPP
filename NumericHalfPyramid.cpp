35d19413c8c9952cd429ca0e4cee76ec25165b42
kushalkumar1362 <kushalkumar1362@gmail.com>
2023-09-03 18:13:27 +0530

    Numeric Half Half Pyramid

--- NumericHalfPyramid.cpp
+++ NumericHalfPyramid.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 /*
-* * * * * 
-* * * *     Inverted Half Pyramid
-* * *       Pattern
-* *
-* 
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
  */
 int main()
 {
